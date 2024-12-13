d174b72f1f34affe9bd8d57ab24301041b10c81c
andybacc <andrea@pant.it>
2024-12-13 22:49:10 +0100

    mix

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 import { Hono } from "hono";
 const app = new Hono();
 
+console.log(`Listening on localhost:3000`);
+
 app.get("/", c => c.text("Hono!"));
 
 export default app;
   
-console.log(`Listening on localhost:${server.port}`);
